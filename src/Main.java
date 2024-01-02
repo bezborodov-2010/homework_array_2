7f03af429f03588c7548ad1111d68139ae713f39
bezborodov-2010 <bezborodov_2010@mail.ru>
2024-01-02 12:05:22 +0700

    Make hw to array_2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1(); task2(); task3();  task4();
+        task1();
+        task2();
+        task3();
+        task4();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -35,8 +39,8 @@ public class Main {
             if (spending > maxSpending) {
                 maxSpending = spending;
             }
-            System.out.println("Минимальная сумма трат за день составила " + minSpending + " рублей");
-            System.out.println("Максимальная сумма трат за день составила " + maxSpending + " рублей");
+            System.out.println("Минимальная сумма трат за день составила " + minSpending + " рублей. Максимальная сумма трат за день составила " + maxSpending + " рублей");
+
         }
     }
 
@@ -48,6 +52,7 @@ public class Main {
         }
         System.out.println("Средняя сумма трат за месяц составила " + (double) sum / arr.length + " рублей");
     }
+
     public static void task4() {
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
